bd2d302028c2472ccb11490d8d5d75a642f88454
lamia <medenglam@windowslive.com>
2019-05-07 08:34:14 +0300

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,8 +19,8 @@ class CommentForm extends Component{
 
     }
     handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId,values.rating, values.author,values.author, values.comment);
     }
     
     toggleModal(){
@@ -117,7 +117,7 @@ class CommentForm extends Component{
                     <div></div>
                 );
         }
-    function RenderComments({comments}){
+    function RenderComments({comments,addComment,dishId}){
             if(comments!=null)
                 return(
                     <div className="col-12 col-md-5 m-1">
@@ -138,7 +138,7 @@ class CommentForm extends Component{
                        
                         } 
                     </ul>
-                    <CommentForm/>
+                    <CommentForm dishId={dishId} addComment={addComment}/>
                     
                     </div>
                 );
@@ -165,7 +165,10 @@ class CommentForm extends Component{
                     </div>
                     <div className="row">
                            <RenderDish dish={props.dish}/>    
-                            <RenderComments comments={props.comments}/>
+                            <RenderComments comments={props.comments}
+                                addComment={props.addComment}
+                                dishId={props.dish.id}
+                            />
                         
                     </div>
                 </div>
